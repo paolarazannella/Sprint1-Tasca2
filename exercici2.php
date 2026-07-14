b32cbdda3a5def30658854b18b154e052d73c2c9
Paola <plara222@gmail.com>
2026-07-14 12:19:33 +0200

    exercici 2 completat

--- exercici2.php
+++ exercici2.php
@@ -1,2 +1,27 @@
 <?php
-echo "Hello, World!";
+
+$string ="Hello, World!";
+
+echo $string;
+echo "<br>";
+
+echo strtoupper($string);
+echo "<br>";
+
+echo strlen($string);
+echo "<br>";
+
+echo strrev($string);
+echo "<br>";
+
+
+$variable = "Aquest es el curs de PHP";
+
+echo $string . " " . $variable;
+
+?>
+
+
+
+
+
