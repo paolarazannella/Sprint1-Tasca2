54f94b0f5a31a2fe2dc16230cca6cea2ee2ae6bd
Paola <plara222@gmail.com>
2026-07-14 11:40:42 +0200

    exercici1 nivell 1 tasca 2

---
+++ exercici1.php
@@ -0,0 +1,21 @@
+<?php
+$int = 24;
+$double = 24.2;
+$string = "Hola";
+$boolean = true;
+
+echo $int;
+echo "<br>";
+echo $double;
+echo "<br>";
+echo $string;
+echo "<br>";
+echo $boolean;
+echo "<br>";
+
+define("mi_nombre", "Paola");
+echo "<h1>";
+echo mi_nombre;
+echo "</h1>";
+
+?>
